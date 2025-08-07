9d3efe06d460a7578f4a1d439b2b8454d710ebf4
28Yadav <karanyadaav001@gmail.com>
2025-08-07 12:41:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ getcontext().prec = 18
 SYMBOLS = ['ETH/USDT:USDT', 'BTC/USDT:USDT']
 TIMEFRAME = '15m'
 ORDER_SIZE_BY_SYMBOL = {
-    'ETH/USDT:USDT': Decimal('0.08')
+    'ETH/USDT:USDT': Decimal('0.09')
 }
 TP_MULTIPLIER = Decimal('2')
 SL_MULTIPLIER = Decimal('2.0')
@@ -229,4 +229,5 @@ if __name__ == '__main__':
                 print(f"[Unhandled Error] {e}")
         print("⏰ Cycle complete, sleeping 60 seconds...")
         time.sleep(30)
+
 
